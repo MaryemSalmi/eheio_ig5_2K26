c36388aef0dfd11632eaa4742da14bcf89945852
maryemsalmi <maryemsalmi380@gmail.com>
2025-11-01 12:08:40 +0100

    Routing : 2ème manière

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class IndexController extends AbstractController{
-   #[Route(path: '/index',name: 'app_index')] 
+   #[Route(path: '/home',name: 'home_index')] 
    public function index(): Response{
     return new Response(content : "👋 Bonjour, ceci est mon premier contrôleur Symfony !");
    }
