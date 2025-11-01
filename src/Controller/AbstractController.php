daa694e3b7853c13a2f0d1d5d68efc47a1ab7de4
maryemsalmi <maryemsalmi380@gmail.com>
2025-11-01 12:40:04 +0100

    Routing : Requirements

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -6,8 +6,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class IndexController extends AbstractController{
-   #[Route(path: '/index/{name}',name: 'app_index')] 
-   public function index(string $name= "Anonymous"): Response{
+   #[Route(path: '/index/{name}',name: 'app_index',requirements: ['name'=>'\d+'])] 
+   public function index(int $name= 1): Response{
     return new Response(content : "Hello" .$name . "!");
    }
 }
