b4c833d54b7d0761937ee9b91af2ccc74afe0df7
maryemsalmi <maryemsalmi380@gmail.com>
2025-11-01 12:20:38 +0100

    Routing : dynamic Url

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -6,9 +6,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class IndexController extends AbstractController{
-   #[Route(path: '/home',name: 'home_index')] 
+   #[Route(path: '/index/{name}',name: 'app_index')] 
    public function index(): Response{
-    return new Response(content : "👋 Bonjour, ceci est mon premier contrôleur Symfony !");
+    return new Response(content : "Hello" .$name . "!");
    }
 }
 
